9fd0552b44efd1eb87b2acb93c1ce6d94c94cc1c
Werick Vieira <werick.vieira@yahoo.com.br>
2018-07-07 14:51:42 -0300

    Melhorar a forma de calcular o total de cada item

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import formatPrice from '../../util/formatPrice';
 import './Cart.scss';
+
+
+function singleSumQuantity(cart, books) {
+  return cart.map(item => (
+    books
+      .filter(book => book.id === item.id)
+      .map((e) => {
+        const { id, name, price } = e;
+        return {
+          id,
+          name,
+          price,
+          quantity: item.quantity,
+          total: e.price * item.quantity,
+        };
+      })[0]
+  ));
+}
+
 
 function Cart({ cart, books }) {
   console.log('- - - - - - - - - - - - -');
@@ -9,9 +29,20 @@ function Cart({ cart, books }) {
   console.log('- - - - - - - - - - - - -');
   console.log('books', books);
   const { length } = cart;
+  const quantitysSum = singleSumQuantity(cart, books);
   return (
     <div className="cart container" hidden={length === 0}>
       <h1>CART</h1>
+      <div className="cart__list">
+        {
+          quantitysSum.map(e => (
+            <div className="cart__item" key={e.id}>
+              <div>{e.name}</div>
+              <span>{formatPrice(e.total)}</span>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
